c4222edd42a7dde9b6c956f75271ba3bb540d41b
Lilly <lillythecommie@protonmail.ch>
2023-06-28 03:11:52 +0300

    Finally added all the directions needed to draw every kind of line, added a limit to how many digits can be read to prevent integer overflow, fixed the position of the error checking if statements, and added command line inputtable iteraction

--- GoL.c
+++ GoL.c
@@ -1,5 +1,4 @@
 // TO DO:
-// IMPLEMENT DIRECTIONAL EDITIİNG
 // SWITCH THE PLACE OF THE VALUE CHECK & ERROR CHECK IN EDITING FUNCTIONS
 // PUT REASONABLE LIMITS ON ALL SIZES 
 // RENAME SOME FUNCTIONS
@@ -16,8 +15,8 @@
 // and this is the easiest method 
 // although this means the functions only work for 2D char arrays of this size
 // thank you C, very cool
-size_t rlen = 0; //row length
-size_t clen = 0; // column length
+int rlen = 0; //row length
+int clen = 0; // column length
 
 // just returns the nth power of 10
 int ten_pow(int n) { 
@@ -30,6 +29,7 @@ int ten_pow(int n) {
 // does the cool printing function
 void grid_print_2(char grid[rlen][clen], int lp) { // finish here 
 	// it doesn't need to print if it's the first time so this is a check
+	static int lrl = 0;
 	static char first_print = 1; 
 	if (first_print) {
 		for (int i = 0; i < rlen; i++) {
@@ -44,7 +44,7 @@ void grid_print_2(char grid[rlen][clen], int lp) { // finish here
 	else {
 		// Goes up #lp (which is always the row count+1 in this program)
 		// this allows us to override all that has been written
-		printf("\033[%dA", lp);  
+		printf("\033[%dA\r", lp);  
 		for (int i = 0; i < rlen; i++) {
 			for (int j = 0; j < clen; j++) { 
 				printf("\033[%dm%d\033[0m ",
@@ -52,11 +52,12 @@ void grid_print_2(char grid[rlen][clen], int lp) { // finish here
 					grid[i][j],
 					lp);
 			}
-			printf("\n");
+			printf("\n", i);
 		}
 	}
+	lrl += 1;
 	first_print = 0;
-	printf("\n");
+	printf("\n%d", lrl);
 }
 
 // prints the entire game grid without ANSI esc codes
@@ -86,6 +87,7 @@ int str2num(char* str, int len, int *ret) {
 	return 0;
 }
 
+#define MAX_IN_BUFF_SIZE 8
 // gets a string SAFELY(ish) to turn into a number and store it in num
 int getnum(int *num) {
 	int nlen; 
@@ -96,8 +98,12 @@ int getnum(int *num) {
 	// because that's how you normally input
 	for (nlen = 0; (n = getc(stdin)) != '\n'; nlen++) {
 		if (nlen > (len-1)) {
-			len *= 2;
-			str = realloc(str, len);
+			if ((len*2) <= MAX_IN_BUFF_SIZE) {	
+				len += 2;
+				str = realloc(str, len);
+			}
+			else 
+				printf("Input digit limit exceeded\n");
 		}
 		if (n == EOF) {
 			printf("EOF while reading\n");
@@ -154,124 +160,179 @@ get_manual_answers:
 // start working on directional input here
 void holine(char grid[rlen][clen]) {
 	int err, x, y, line_len;
-	// int lr;
+	int lr;
 get_holine_in:
-	/*printf("Should the line be drawn Left to Right (1) or Right to Left (2): ");
+	printf("Should the line be drawn Left to Right (1) or Right to Left (2): ");
 	err = getnum(&lr);
-	if*/ 
-	printf("Input the X coordinates: ");
-	err = getnum(&x);
-	if (x >= clen || x < 0) {
-		printf("Invalid X coordinates for line beginning\n");
+	if (err == -1) {
+		printf("Error getting value\n");
 		goto get_holine_in;
 	}
-	else if (err == -1) {
+	if (lr != 1 && lr != 2) {
+		printf("Invalid option\n");
+		goto get_holine_in;
+	}
+
+	printf("Input the X coordinates: ");
+	err = getnum(&x);
+	if (err == -1) {
 		printf("Error getting value\n");
+		goto get_holine_in;
+	}
+	if (x >= clen || x < 0) {
+		printf("Invalid X coordinates for line beginning\n");
 		goto get_holine_in;
 	}
 	
 	printf("Input the Y coordinates: ");
 	err = getnum(&y);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_holine_in;
+	}
 	if (y >= rlen || y < 0) {
 		printf("Invalid Y coordinates for line beginning\n");	
 		goto get_holine_in;
 	}
-	else if (err == -1) {
-		printf("Error getting value\n");
-		goto get_holine_in; 
-	}
 	
 	printf("Input the line length: ");
 	err = getnum(&line_len);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_holine_in;
+	}
 	if (line_len > clen || line_len < 0) {
 		printf("Invalid length for horizontal line\n");
 		goto get_holine_in;
 	}
-	else if (err == -1) {
-		printf("Error getting value\n");
-		goto get_holine_in; 
+	
+	if (lr == 1) {
+		for(int i = 0; i < line_len; i++)
+			grid[y][(x+i) % clen] = 1;
 	}
-
-	for(int i = 0; i < line_len; i++)
-		grid[y][(x+i) % clen] = 1;
+	else {
+		for(int i = 0; i < line_len; i++)
+			grid[y][((x-i)<0) ? (clen - i) : ((x-i) % clen)] = 1;
+	}
 }
 
 void verline(char grid[rlen][clen]) {
-	int err, x, y, line_len;
+	int err, lr, x, y, line_len;
 get_verline_in:
+	printf("Should the line be drawn Top to Bottom (1) or Bottom to Top (2): ");
+	err = getnum(&lr);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_verline_in;
+	}
+	if (lr != 1 && lr != 2) {
+		printf("Invalid option\n");
+		goto get_verline_in;
+	}
+	
 	printf("Input the X coordinates: ");
 	err = getnum(&x);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_verline_in; 
+	}
 	if (x >= clen || x < 0) {
 		printf("Invalid X coordinates for line beginning\n");
 		goto get_verline_in;
 	}
-	else if (err == -1) {
-		printf("Error getting value\n");
-		goto get_verline_in; 
-	}
 	
 	printf("Input the Y coordinates: ");
 	err = getnum(&y);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_verline_in;	
+	}
 	if (y >= rlen || y < 0) {
 		printf("Invalid Y coordinates for line beginning\n");
 		goto get_verline_in;
-	}
-	else if (err == -1) {
-		printf("Error getting value\n");
-		goto get_verline_in;	
 	}
 	
 	printf("Input the line length: ");
 	err = getnum(&line_len);
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_verline_in;	
+	}
 	if (line_len > rlen || line_len < 0) {
 		printf("Invalid length for horizontal line\n");
 		goto get_verline_in;
 	}
-	else if (err == -1) {
-		printf("Error getting value\n");
-		goto get_verline_in;	
-	}
 
-	for(int i = 0; i < line_len; i++)
-		grid[(y+i) % rlen][x] = 1;
+	if (lr == 1) {
+		for(int i = 0; i < line_len; i++)
+			grid[(y+i) % rlen][x] = 1;
+	}
+	else {
+		for(int i = 0; i < line_len; i++)
+			grid[((y-i)<0) ? (rlen - i) : ((y-i) % rlen)][x] = 1;
+	}
 }
 
 
 void dialine(char grid[rlen][clen]) {
-	int err, x, y, line_len;
-get_dialine_in:
-	printf("Input the X coordinates: ");
-	err = getnum(&x);
-	if (x >= clen || x < 0) {
-		printf("Invalid X coordinates for line beginning\n");
+	int err, lr, x, y, line_len;
+get_dialine_in:	
+	printf("Should the line be drawn Top Left to Bottom Right (1) or Top Right to Bottom Left (2): ");
+	err = getnum(&lr);
+	if (err == -1) {
+		printf("Error getting value\n");
 		goto get_dialine_in;
 	}
-	else if (err == -1) {
+	if (lr != 1 && lr != 2) {
+		printf("Invalid option\n");
+		goto get_dialine_in;
+	}
+	
+	printf("Input the X coordinates: ");
+	err = getnum(&x);
+	if (err == -1) {
 		printf("Error getting value\n");
+		goto get_dialine_in;
+	}
+	if (x >= clen || x < 0) {
+		printf("Invalid X coordinates for line beginning\n");
 		goto get_dialine_in;
 	}
 	
 	printf("Input the Y coordinates: ");
 	err = getnum(&y);
-	if (y >= rlen || y < 0) {
-		printf("Invalid Y coordinates for line beginning\n");
+	if (err == -1) {
+		printf("Error getting value\n");
 		goto get_dialine_in;
 	}
-	else if (err == -1) {
-		printf("Error getting value\n");
+	if (y >= rlen || y < 0) {
+		printf("Invalid Y coordinates for line beginning\n");
 		goto get_dialine_in;
 	}
 	
 	printf("Input the line length: ");
 	err = getnum(&line_len);
 	int maxL = (int)sqrt((double)(clen*clen + rlen*rlen));
+	if (err == -1) {
+		printf("Error getting value\n");
+		goto get_dialine_in;
+	}
 	if (line_len > maxL || line_len < 0) {
 		printf("Invalid length for horizontal line\n");
 		goto get_dialine_in;
 	}
 
-	for(int i = 0; i < line_len; i++)
-		grid[(y+i) % rlen][(x+i) % clen] = 1;
+	if (lr == 1) {
+		for(int i = 0; i < line_len; i++)
+			grid[(y+i) % rlen][(x+i) % clen] = 1;
+	}
+	else {
+		int i = 0;
+		while (i < line_len) {
+			grid[(y+i) % rlen][((x-i)<0) ? (clen - i) : ((x-i) % clen)] = 1;
+			i += 1;
+		}
+	}
 }
 
 void draw_cell_line(char grid[rlen][clen]) {
@@ -353,20 +414,31 @@ void game_of_life(char arr[rlen][clen]) {
 		}
 	}
 	grid_print_2(arr, rlen+1);
-	usleep(450000);
+	usleep(500000);
 }
 
 int main(int argc, char ** argv) {
-	// get the row & column size for the player grid
-	str2num(argv[1], strlen(argv[1]), &rlen);
-	str2num(argv[2], strlen(argv[2]), &clen);
-	
-	// if either number is smaller than or equal 0
-	// that's no good
-	if (!(rlen > 0 && clen > 0)) {
-		printf("Ivalid grid size parameters\n");
+	if (argc != 4) {
+		printf("usage: ./GoL [row_count] [column count] [iteration count]");
 		return -1;
 	}
+	int ic;
+
+	// get the row & column size for the player grid, and number of iterations
+	int err = str2num(argv[1], strlen(argv[1]), &rlen);
+	err += str2num(argv[2], strlen(argv[2]), &clen);
+	err += str2num(argv[3], strlen(argv[3]), &ic);	
+	// if either number is smaller than or equal 0
+	// that's no good
+	if (err < 0) {
+		printf("Error getting cl arguments\n");
+		return -1;
+	}
+	if (!(rlen > 0 && clen > 0 && ic > 0)) {
+		printf("Ivalid grid size parameters or iteration count\n");
+		return -1;
+	}
+
 	// create a local array
 	// that really should have been a global pointer
 	char grid[rlen][clen];
@@ -380,7 +452,9 @@ int main(int argc, char ** argv) {
 	}
 	
 	edit_grid(grid);
-	printf(/*"\033[2J\033[H*/"The Game: \n");
-	for (int i = 0; i < 10; i++)
+	printf("\033[2J\033[HThe Game: \n");
+	for (int i = 0; i < ic; i++)
 		game_of_life(grid);
+
+	return 0;
 }
